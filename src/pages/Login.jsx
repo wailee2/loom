54f7e8b611c40b882e3c9100509b2f78800ed813
Feranmi <9fernni@gmail.com>
2025-09-08 20:06:45 +0100

    testing login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
     <form onSubmit={handleSubmit}>
       <input
         type="email"
-        placeholder="Email"
+        placeholder="Emailferanmi"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
         required
