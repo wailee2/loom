63cffe4e19b2bfdcfdd2a7c49a616654fb7c61ca
Feranmi <9fernni@gmail.com>
2025-09-09 00:54:46 +0100

    this is the good code ready to go

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -609,7 +609,7 @@ const Register = () => {
               onClick={handleBack}
               className="rounded-lg bg-gray-200 px-4 py-2 text-sm font-medium text-gray-700"
             >
-              Back
+              Back.this 
             </button>
           )}
           {step < 5 && (
